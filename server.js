b59f0707acf072801ff5dfabe4954254db709d66
Nikita Rastorguev <nirastor@gmail.com>
2021-05-27 20:53:22 +0300

    initial commit

---
+++ server.js
@@ -0,0 +1,105 @@
+const http = require('http');
+const Koa = require('koa');
+const Router = require('koa-router');
+const cors = require('koa2-cors');
+const koaBody = require('koa-body');
+
+const app = new Koa();
+
+app.use(cors());
+app.use(koaBody({json: true}));
+
+const tickets = [
+    {
+      id: 1,
+      status: false,
+      name: 'Задача 1',
+      description: 'Описание задачи один',
+      created: new Date('2021-05-21, 12:45'),
+    },
+    {
+      id: 2,
+      status: false,
+      name: 'Вторая таска',
+      description: 'И её большое описание\n1.сначал это\n2.потом другое',
+      created: new Date('2021-05-17, 11:17'),
+    },
+  ];
+
+const router = new Router();
+
+// task http
+router.get('/allTickets', async (ctx, next) => {
+    ctx.response.body = tickets.map(t => {delete t.description; return t});
+});
+router.get('/ticketById', async (ctx, next) => {
+    const ticketId = Number(ctx.request.query.id);
+    ctx.response.body = tickets.find(t => t.id === ticketId);
+});
+
+// task different state of loading (demo on hooks)
+// router.get('/data', async (ctx, next) => {
+//     ctx.response.body = {status: "ok"};
+// });
+// router.get('/error', async (ctx, next) => {
+//     ctx.response.status = 500;
+//     ctx.response.body = {status: "Internal Error"};
+// });
+// router.get('/loading', async (ctx, next) => {
+//     await new Promise(resolve => {
+//         setTimeout(() => {
+//             resolve();
+//         }, 5000);
+//     });
+//     ctx.response.body = {status: "ok"};
+// });
+
+// task work with http (notes manager)
+
+// router.get('/notes/nextid', async (ctx, next) => {
+//     ctx.response.body = nextId;
+// });
+// router.post('/notes', async(ctx, next) => {
+//     notes.push({...ctx.request.body, id: nextId++});
+//     ctx.response.status = 204;
+// });
+// router.delete('/notes/:id', async(ctx, next) => {
+//     const noteId = Number(ctx.params.id);
+//     const index = notes.findIndex(o => o.id === noteId);
+//     if (index !== -1) {
+//         notes.splice(index, 1);
+//     }
+//     ctx.response.status = 204;
+// });
+
+// task router (like facebook)
+// router.get('/posts', async (ctx, next) => {
+//     ctx.response.body = posts;
+// });
+// router.get('/posts/nextid', async (ctx, next) => {
+//     ctx.response.body = nextId;
+// });
+// router.post('/posts', async(ctx, next) => {
+//     const {id, content} = ctx.request.body;
+//     if (id !== 0) {
+//         posts = posts.map(o => o.id !== id ? o : {...o, content: content});
+//         ctx.response.status = 204;
+//         return;
+//     }
+//     posts.push({...ctx.request.body, id: nextId++, created: Date.now()});
+//     ctx.response.status = 204;
+// });
+// router.delete('/posts/:id', async(ctx, next) => {
+//     const postId = Number(ctx.params.id);
+//     const index = posts.findIndex(o => o.id === postId);
+//     if (index !== -1) {
+//         posts.splice(index, 1);
+//     }
+//     ctx.response.status = 204;
+// });
+
+app.use(router.routes()).use(router.allowedMethods());
+
+const port = 7778;
+const server = http.createServer(app.callback());
+server.listen(port, () => console.log('server started'));
