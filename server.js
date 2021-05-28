5b4795bd5b03724afb3bdeb21bfabe44505ba3cf
Nikita Rastorguev <nirastor@gmail.com>
2021-05-28 15:59:13 +0300

    14 — clear comments

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ const router = new Router();
 
 // task http
 router.get('/apiVersion', async (ctx, next) => {
-    ctx.response.body = '13 — edit ticket';
+    ctx.response.body = '14 — clear comments';
 });
 router.get('/allTickets', async (ctx, next) => {
     ctx.response.body = tickets.map(t => ({
@@ -80,67 +80,6 @@ router.post('/editTicket', async(ctx, next) => {
     ticketForEdit.description = description
     ctx.response.status = 204;
 });
-
-// task different state of loading (demo on hooks)
-// router.get('/data', async (ctx, next) => {
-//     ctx.response.body = {status: "ok"};
-// });
-// router.get('/error', async (ctx, next) => {
-//     ctx.response.status = 500;
-//     ctx.response.body = {status: "Internal Error"};
-// });
-// router.get('/loading', async (ctx, next) => {
-//     await new Promise(resolve => {
-//         setTimeout(() => {
-//             resolve();
-//         }, 5000);
-//     });
-//     ctx.response.body = {status: "ok"};
-// });
-
-// task work with http (notes manager)
-
-// router.get('/notes/nextid', async (ctx, next) => {
-//     ctx.response.body = nextId;
-// });
-// router.post('/notes', async(ctx, next) => {
-//     notes.push({...ctx.request.body, id: nextId++});
-//     ctx.response.status = 204;
-// });
-// router.delete('/notes/:id', async(ctx, next) => {
-//     const noteId = Number(ctx.params.id);
-//     const index = notes.findIndex(o => o.id === noteId);
-//     if (index !== -1) {
-//         notes.splice(index, 1);
-//     }
-//     ctx.response.status = 204;
-// });
-
-// task router (like facebook)
-// router.get('/posts', async (ctx, next) => {
-//     ctx.response.body = posts;
-// });
-// router.get('/posts/nextid', async (ctx, next) => {
-//     ctx.response.body = nextId;
-// });
-// router.post('/posts', async(ctx, next) => {
-//     const {id, content} = ctx.request.body;
-//     if (id !== 0) {
-//         posts = posts.map(o => o.id !== id ? o : {...o, content: content});
-//         ctx.response.status = 204;
-//         return;
-//     }
-//     posts.push({...ctx.request.body, id: nextId++, created: Date.now()});
-//     ctx.response.status = 204;
-// });
-// router.delete('/posts/:id', async(ctx, next) => {
-//     const postId = Number(ctx.params.id);
-//     const index = posts.findIndex(o => o.id === postId);
-//     if (index !== -1) {
-//         posts.splice(index, 1);
-//     }
-//     ctx.response.status = 204;
-// });
 
 app.use(router.routes()).use(router.allowedMethods());
 
